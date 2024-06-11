1ed2867d12d38272c1c1fff045dd447f0ec65a4c
Nabil_saadi <nabil.saadi@codersdc.com>
2024-06-11 12:47:13 +0300

    nike done

--- lib/core/data/helpers/data_provider.dart
+++ lib/core/data/helpers/data_provider.dart
@@ -272,7 +272,61 @@ toObject();
             "classValue": "size",
           },
         },
-      ]
+      ],
+      "script": '''
+
+function getTitles() {
+  return Array.from(document.dyQuerySelectorAll("div > div > div > div > div > div > div.space-y-3 > a > h3")).map((x)=>x.textContent);
+}
+
+function getQuantities() {
+  return Array.from(document.querySelectorAll("div > div > div > div > div > div > div.space-y-3 > div > div.flex.w-24 > div > div > select")).map((x)=>x.value);
+}
+
+function getImages() {
+  return Array.from(document.querySelectorAll("div > div > div > div > div > a > div > img")).map((x)=>x.src);
+}
+
+function getPrices() {
+  return Array.from(document.querySelectorAll("div > div > div > div > div > div > div > p > span[data-test-id='item-price']")).map((x)=>x.textContent);
+}
+
+function getUrls() {
+  return Array.from(document.querySelectorAll("div > div > div > div > div > a[data-test-id='cart-item-img-link']")).map((x)=>x.href);
+}
+
+function getColors() {
+  return Array.from(document.querySelectorAll("div > div > div > div > div > div > div > p > span[data-test-id='color']")).map((x)=>x.textContent);
+}
+
+function getSizes() {
+  return Array.from(document.querySelectorAll("div > div > div > div > div > div > div > p > span[data-test-id='size']")).map((x)=>x.textContent);
+}
+
+function toObject(){
+
+    var dict = {
+        titles: [],
+        images: [],
+        prices: [],
+        urls: [],
+        colors: [],
+        sizes: [],
+        quantities: []
+    };
+
+    dict.titles.push.apply(dict.titles, getTitles());
+    dict.images.push.apply(dict.images, getImages());
+    dict.prices.push.apply(dict.prices, getPrices());
+    dict.urls.push.apply(dict.urls, getUrls());
+    dict.colors.push.apply(dict.colors, getColors());
+    dict.sizes.push.apply(dict.sizes, getSizes());
+    dict.quantities.push.apply(dict.quantities, getQuantities());
+    return dict;
+
+}
+toObject();
+'''
     }),
     // - Nike
     ShoppingWebsite.fromJson(const {
@@ -283,7 +337,61 @@ toObject();
           "https://api.weorder.app/assets/uploads/media-uploader/1702556484833.jpeg",
       "is_free": 1,
       "status": 1,
-      "vendor": "nike"
+      "vendor": "nike",
+      "script": '''
+
+function getTitles() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > div > div.css-18o14p5.ezci20q3 > div.css-1u52liu.ezci20q1 > a > h2")).map((x)=>x.textContent);
+}
+
+function getQuantities() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > div > div.css-18o14p5.ezci20q3 > div.css-1u52liu.ezci20q1 > div.css-3x77rp.eyas8011 > div:nth-child(2) > div > select")).map((x)=>x.value);
+}
+
+function getImages() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > figure > a > picture > img")).map((x)=>x.src);
+}
+
+function getPrices() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > div > div.css-18o14p5.ezci20q3 > div.css-q2xuk9.ezci20q0 > p > span > span > span")).map((x)=>x.childNodes[1].textContent);
+}
+
+function getUrls() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > figure > a")).map((x)=>x.href);
+}
+
+function getColors() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > div > div.css-18o14p5.ezci20q3 > div.css-1u52liu.ezci20q1 > div.css-1f31asj.eneqjw10")).map((x)=>x.textContent);
+}
+
+function getSizes() {
+  return Array.from(document.querySelectorAll("div.css-k008qs.ei1batk0 > div > div.css-18o14p5.ezci20q3 > div.css-1u52liu.ezci20q1 > div.css-3x77rp.eyas8011 > div:nth-child(1) > div > select")).map((x)=>x.options[x.selectedIndex].text);
+}
+
+function toObject(){
+
+    var dict = {
+        titles: [],
+        images: [],
+        prices: [],
+        urls: [],
+        colors: [],
+        sizes: [],
+        quantities: []
+    };
+
+    dict.titles.push.apply(dict.titles, getTitles());
+    dict.images.push.apply(dict.images, getImages());
+    dict.prices.push.apply(dict.prices, getPrices());
+    dict.urls.push.apply(dict.urls, getUrls());
+    dict.colors.push.apply(dict.colors, getColors());
+    dict.sizes.push.apply(dict.sizes, getSizes());
+    dict.quantities.push.apply(dict.quantities, getQuantities());
+    return dict;
+
+}
+toObject();
+'''
     }),
     // - She-in
     ShoppingWebsite.fromJson(const {
@@ -315,4 +423,21 @@ toObject();
       keys: [],
     ),
   ];
+
+  ShoppingWebsiteScript script() => ShoppingWebsiteScriptDTO.fromMap({
+        "titles":
+            'return Array.from(document.querySelectorAll("div > div.line-item___i5rwj.line-item__border-redesign___cp6Bq > div.line-item__details-wrapper___hdXUf.col-s-7.col-m-7.col-l-12.offset-l-1.no-gutters > div.line-item__details-row___hV7vL.row > div.line-item__details___HibzD > div > div:nth-child(1) > div > a > span")).map((x)=> x.textContent);',
+        "quantities":
+            'Array.from(document.querySelectorAll("div > div.line-item__details-wrapper___hdXUf.col-s-7.col-m-7.col-l-12.offset-l-1.no-gutters > div.line-item__footer___UcQo4.row.gl-align-items-center > div.dropdown-container___mjvVf > div > div > select")).map((x)=>x.value);',
+        "images":
+            'Array.from(document.querySelectorAll("div > div > div.no-gutters.col-s-5.line-item__image-sizing-wrapper___a91cW > a > img")).map((x)=>x.src);',
+        "prices":
+            'Array.from(document.querySelectorAll("div > div.line-item__details-wrapper___hdXUf.col-s-7.col-m-7.col-l-12.offset-l-1.no-gutters > div.line-item__footer___UcQo4.row.gl-align-items-center > div.gl-hidden-m > div > div")).map((x)=>x.textContent);',
+        "urls":
+            'Array.from(document.querySelectorAll("div > div > div.no-gutters.col-s-5.line-item__image-sizing-wrapper___a91cW > a")).map((x)=>x.href);',
+        "colors":
+            'Array.from(document.querySelectorAll("div > div.line-item__details-wrapper___hdXUf.col-s-7.col-m-7.col-l-12.offset-l-1.no-gutters > div.line-item__details-row___hV7vL.row > div.line-item__details___HibzD > div > div:nth-child(2) > div > span")).map((x)=>x.textContent);',
+        "sizes":
+            'Array.from(document.querySelectorAll("div > div.line-item__details-wrapper___hdXUf.col-s-7.col-m-7.col-l-12.offset-l-1.no-gutters > div.line-item__details-row___hV7vL.row > div.line-item__details___HibzD > div > div.gl-vspace > div > div > span:nth-child(2)")).map((x)=>x.textContent);'
+      }).toModel();
 }
--- lib/core/domain/models/shopping_website.dart
+++ lib/core/domain/models/shopping_website.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 //
 
-import 'package:drop_shopping_app/core/domain/models/cart_item_keys.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:drop_shopping_app/core/domain/models/cart_item_keys.dart';
 
 import 'shopping_website_vendor.dart';
 
@@ -45,4 +46,121 @@ class ShoppingWebsite extends Equatable {
       script: map['script'],
     );
   }
+}
+
+class ShoppingWebsiteScriptDTO {
+  String? titles;
+  String? quantities;
+  String? images;
+  String? prices;
+  String? urls;
+  String? colors;
+  String? sizes;
+
+  ShoppingWebsiteScriptDTO({
+    this.titles,
+    this.quantities,
+    this.images,
+    this.prices,
+    this.urls,
+    this.colors,
+    this.sizes,
+  });
+
+  factory ShoppingWebsiteScriptDTO.fromMap(Map<String, dynamic> map) {
+    return ShoppingWebsiteScriptDTO(
+      titles: map['titles'] != null ? map['titles'] as String : null,
+      quantities:
+          map['quantities'] != null ? map['quantities'] as String : null,
+      images: map['images'] != null ? map['images'] as String : null,
+      prices: map['prices'] != null ? map['prices'] as String : null,
+      urls: map['urls'] != null ? map['urls'] as String : null,
+      colors: map['colors'] != null ? map['colors'] as String : null,
+      sizes: map['sizes'] != null ? map['sizes'] as String : null,
+    );
+  }
+
+  ShoppingWebsiteScript toModel() => ShoppingWebsiteScript(
+        titles: titles ?? '[]',
+        quantities: quantities ?? '[]',
+        images: images ?? '[]',
+        prices: prices ?? '[]',
+        urls: urls ?? '[]',
+        colors: colors ?? '[]',
+        sizes: sizes ?? '[]',
+      );
+}
+
+class ShoppingWebsiteScript {
+  final String titles;
+  final String quantities;
+  final String images;
+  final String prices;
+  final String urls;
+  final String colors;
+  final String sizes;
+  ShoppingWebsiteScript({
+    required this.titles,
+    required this.quantities,
+    required this.images,
+    required this.prices,
+    required this.urls,
+    required this.colors,
+    required this.sizes,
+  });
+
+  String get script => '''
+
+function getTitles() {
+  return $titles
+}
+
+function getQuantities() {
+  return $quantities
+}
+
+function getImages() {
+  return $images
+}
+
+function getPrices() {
+  return $prices
+}
+
+function getUrls() {
+  return $urls
+}
+
+function getColors() {
+  return $colors
+}
+
+function getSizes() {
+  return $sizes
+}
+
+function toObject(){
+
+    var dict = {
+        titles: [],
+        images: [],
+        prices: [],
+        urls: [],
+        colors: [],
+        sizes: [],
+        quantities: []
+    };
+
+    dict.titles.push.apply(dict.titles, getTitles());
+    dict.images.push.apply(dict.images, getImages());
+    dict.prices.push.apply(dict.prices, getPrices());
+    dict.urls.push.apply(dict.urls, getUrls());
+    dict.colors.push.apply(dict.colors, getColors());
+    dict.sizes.push.apply(dict.sizes, getSizes());
+    dict.quantities.push.apply(dict.quantities, getQuantities());
+    return dict;
+
+}
+toObject();
+''';
 }
